acbb7ac2d41577cc7932c8fa44c89ce115d9098a
nbanshchikov <rn3kk.nick@gmail.com>
2023-11-25 18:34:53 +0300

    -работае обработка входящих подключений и вычитывание данных из них

---
+++ src/Logger.cpp
@@ -0,0 +1,108 @@
+#include "Logger.h"
+#include <pthread.h>
+#include <queue>
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <mutex>
+using namespace std;
+
+static queue<string> * log_queue = nullptr;
+queue<string> consoleQueue;
+static int queueCount = 0;
+static pthread_t logThreadId = 0;
+static bool terminate = false;
+
+void *loggerThread(void* vargp);
+
+void initLogger(int count_threads)
+{
+  log_queue = new queue<string>[count_threads];
+  queueCount = count_threads;
+  pthread_create(&logThreadId, 0x0, loggerThread, 0x0);
+}
+
+void destroyLogger()
+{
+  if (log_queue){
+    delete [] log_queue;
+    log_queue = nullptr;
+  }
+  if(logThreadId){
+    ::terminate = true;
+    pthread_join(logThreadId, 0x0);
+  }
+}
+
+
+void addMessage(int queNum, std::string msg)
+{
+  if (queNum > queueCount)
+    return;
+  log_queue[queNum].push(msg);
+}
+
+void *loggerThread(void* vargp)
+{
+  ofstream f;
+  f.open ("bi4connector.log", std::ios_base::app);
+
+  while(!::terminate)
+  {
+    for(int i=0; i< queueCount; ++i){
+      if (log_queue[i].empty())
+        continue;
+      string data = log_queue[i].front();
+      log_queue[i].pop();
+      if(data.length() >0){
+        f << i << " " << data << "\n";
+      }
+    }
+    f.flush();
+    if(!consoleQueue.empty())
+    {
+      string data = consoleQueue.front();
+      consoleQueue.pop();
+      printf(data.c_str());
+    }
+    usleep(10000);
+  }
+  f.close();
+  return nullptr;
+}
+
+void lDebug(int queNum, std::string msg)
+{
+  addMessage(queNum, string("[DEBUG] ") + msg);
+}
+
+void lInfo(int queNum, std::string msg)
+{
+  addMessage(queNum, string("[INFO] ") + msg);
+}
+
+void lError(int queNum, std::string msg)
+{
+  addMessage(queNum, string("[ERROR] ") + msg);
+}
+
+void lConsole(std::string msg)
+{
+  consoleQueue.push(msg);
+}
+
+std::mutex mutexErrorLog;
+void toStatusLog(int thId, std::string msg)
+{
+  toStatusLog(to_string(thId) + " " + msg);
+}
+
+void toStatusLog(std::string msg)
+{
+  mutexErrorLog.lock();
+  ofstream f;
+  f.open ("bi4connector_status.log", std::ios_base::app);
+  f << msg << "\n";
+  f.close();
+  mutexErrorLog.unlock();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,36 @@
-#include <pthread.h>
-#include <sys/epoll.h>
-
+#include "Logger.h"
 #include <algorithm>
+#include <arpa/inet.h>
+#include <cstring>
 #include <errno.h>
 #include <fcntl.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <pthread.h>
 #include <set>
 #include <stdio.h>
+#include <sys/epoll.h>
+#include <sys/signal.h>
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+#define check(expr) \
+  if (!(expr)) { \
+    perror(#expr); \
+    kill(0, SIGTERM); \
+  }
+
 using namespace std;
 
 int epoll_listen = 0;
-int epoll_read = 0;
+
+int listen_sock = 0;
 
 void error(const char *msg)
 {
-  perror(msg);
+  toStatusLog(string(msg) + " " + std::strerror(errno));
   exit(1);
 }
 
@@ -26,96 +38,103 @@ bool terminate = false;
 
 #define MAX_EVENTS 10
 
-void setnonblocking(int fd) { fcntl(fd, F_SETFL, O_NONBLOCK); }
+void setnonblocking(int fd)
+{
+  fcntl(fd, F_SETFL, O_NONBLOCK);
+}
+
+void enable_keepalive(int sock)
+{
+  int yes = 1;
+  check(setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, &yes, sizeof(int)) != -1);
+
+  int idle = 1;
+  check(setsockopt(sock, IPPROTO_TCP, TCP_KEEPIDLE, &idle, sizeof(int)) != -1);
+
+  int interval = 1;
+  check(setsockopt(sock, IPPROTO_TCP, TCP_KEEPINTVL, &interval, sizeof(int)) != -1);
+
+  int maxpkt = 6;
+  check(setsockopt(sock, IPPROTO_TCP, TCP_KEEPCNT, &maxpkt, sizeof(int)) != -1);
+}
 
 void *epoll_server(void *vargp)
 {
-  struct sockaddr_in addr;
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(3333);
-  addr.sin_addr.s_addr = INADDR_ANY;
-  socklen_t adr_len = sizeof(addr);
+  int tId = (size_t) vargp;
+  lDebug(tId, "Thread is start: " + to_string(tId));
+  toStatusLog(tId, "Start thread");
+  int epollfd;
+  epollfd = epoll_create1(0);
+  if (epollfd == -1) {
+    error("epoll_create1");
+    exit(EXIT_FAILURE);
+  }
 
-  struct epoll_event ev;
   struct epoll_event events[MAX_EVENTS];
-  int listen_sock, conn_sock, nfds, epollfd;
-  listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (listen_sock < 0)
-  {
-    perror("socket");
-    exit(1);
-  }
-
-  const int enable = 1;
-  if (setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
-    error("setsockopt(SO_REUSEADDR) failed");
-
-  if (bind(listen_sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-  {
-    perror("bind");
-    exit(2);
-  }
-
-  if (listen(listen_sock, 100) < 0)
-  {
-    perror("listen");
-    exit(3);
-  }
-
-  fcntl(listen_sock, F_SETFL, O_NONBLOCK);
-
-  epollfd = epoll_create(1024);
-  if (epollfd == -1)
-  {
-    perror("epoll_create1");
-    exit(EXIT_FAILURE);
-  }
-
-  ev.events = EPOLLIN | EPOLLERR;
+  struct epoll_event ev = {0};
+  ev.events = EPOLLIN | EPOLLERR | EPOLLEXCLUSIVE;
   ev.data.fd = listen_sock;
-  if (epoll_ctl(epollfd, EPOLL_CTL_ADD, listen_sock, &ev) == -1)
-  {
-    perror("epoll_ctl: listen_sock");
+  if (epoll_ctl(epollfd, EPOLL_CTL_ADD, listen_sock, &ev) == -1) {
+    error("epoll_ctl: listen_sock");
     exit(EXIT_FAILURE);
   }
 
-  struct sockaddr in_addr;
+  struct sockaddr_in in_addr;
   socklen_t in_addr_len = sizeof(in_addr);
 
-  for (;;)
-  {
-    nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
-    if (nfds == -1)
-    {
-      perror("epoll_wait");
+  for (;;) {
+//    toStatusLog(tId, "Next run epoll_wait()");
+    int nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+    if (nfds == -1) {
+      error("epoll_wait");
       exit(EXIT_FAILURE);
     }
+//    toStatusLog(tId, "Have event");
+    for (int n = 0; n < nfds; ++n) {
+      if (events[n].data.fd == listen_sock) {
+        int conn_sock = accept(listen_sock, (struct sockaddr *) &in_addr, &in_addr_len);
+        if (conn_sock >= 0)
+        {
+          char *ip = inet_ntoa(in_addr.sin_addr);
+          uint16_t port = htons(in_addr.sin_port);
+          lInfo(tId, "Input connection " + string(ip) + " " + to_string(port));
 
-    for (int n = 0; n < nfds; ++n)
-    {
-      if (events[n].data.fd == listen_sock)
-      {
-        conn_sock = accept(listen_sock, (struct sockaddr *)&in_addr, &in_addr_len);
-        if (conn_sock == -1)
-        {
-          perror("accept");
-          exit(EXIT_FAILURE);
+          setnonblocking(conn_sock);
+          enable_keepalive(conn_sock);
+          ev.events = EPOLLIN | EPOLLET | EPOLLONESHOT;
+          ev.data.fd = conn_sock;
+          if (epoll_ctl(epollfd, EPOLL_CTL_ADD, conn_sock, &ev) == -1) {
+            perror("epoll_ctl: conn_sock");
+            exit(EXIT_FAILURE);
+          }
         }
-        setnonblocking(conn_sock);
-        ev.events = EPOLLIN | EPOLLET;
-        ev.data.fd = conn_sock;
-        if (epoll_ctl(epollfd, EPOLL_CTL_ADD, conn_sock, &ev) == -1)
-        {
-          perror("epoll_ctl: conn_sock");
-          exit(EXIT_FAILURE);
+        else if(errno == EAGAIN) {
+          lError(tId, "Server thread ***FAILED*** to proccess accept with EAGAIN");
         }
-      }
-      else
-      {
+      } else if (events[n].events & EPOLLIN) {
         char buf[512];
-        int rbytes = read(events[n].data.fd, &buf, 512);
-        printf("read bytes %d\n", rbytes);
-        send(events[n].data.fd, &buf, rbytes, 0x0);
+        while (1) {
+          int rbytes = read(events[n].data.fd, &buf, 512);
+          if (rbytes > 0) {
+            send(events[n].data.fd, &buf, rbytes, 0x0);
+          }
+          if (errno == EAGAIN) {
+            ev.events = EPOLLIN | EPOLLET | EPOLLONESHOT;
+            ev.data.fd = events[n].data.fd;
+            if (epoll_ctl(epollfd, EPOLL_CTL_MOD, events[n].data.fd, &ev) == -1) {
+              error("epoll_ctl: events[n].data.fd");
+              exit(EXIT_FAILURE);
+            }
+            break;
+          }
+        }
+      } else {
+      }
+
+      if (events[n].events & (EPOLLRDHUP | EPOLLHUP)) {
+        lInfo(tId, "connection closed ");
+        epoll_ctl(epollfd, EPOLL_CTL_DEL, events[n].data.fd, NULL);
+        close(events[n].data.fd);
       }
     }
   }
@@ -124,8 +143,52 @@ void *epoll_server(void *vargp)
 
 int main(int argc, char *argv[])
 {
-  pthread_t serverThreadId;
-  pthread_create(&serverThreadId, 0x0, epoll_server, 0x0);
-  pthread_join(serverThreadId, 0x0);
+
+  int n = 6;
+  initLogger(n);
+  toStatusLog("App is start");
+
+  struct sockaddr_in addr;
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(3333);
+  addr.sin_addr.s_addr = INADDR_ANY;
+  socklen_t adr_len = sizeof(addr);
+
+  toStatusLog("Next start listen port");
+  listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (listen_sock < 0) {
+    error("socket");
+    exit(1);
+  }
+
+  const int enable = 1;
+  if (setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+    error("setsockopt(SO_REUSEADDR) failed");
+
+  toStatusLog("Next start bind port");
+  if (bind(listen_sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+    error("bind");
+    exit(2);
+  }
+
+  if (listen(listen_sock, 3) < 0) {
+    error("listen");
+    exit(3);
+  }
+
+  fcntl(listen_sock, F_SETFL, O_NONBLOCK);
+
+  pthread_t serverThreadId[n];
+  for (size_t i = 0; i < n; i++) {
+    toStatusLog("Create thread " + to_string(i));
+    pthread_create(serverThreadId + i, 0x0, epoll_server, (void *) i);
+  }
+  for (int i = 0; i < n; i++) {
+    toStatusLog("Join thread " + to_string(i));
+    pthread_join(serverThreadId[i], 0x0);
+    lDebug(i, "Thread id is join end");
+  }
+  lInfo(0, "App is end");
+  destroyLogger();
   return (0);
 }
